643acd9753c55d1cb80ed90e6cd04ac107acaea5
Lauren Stone <lstone321@gmail.com>
2020-07-15 00:31:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,6 @@
 #   puts "Hello, my name is #{name}."
 # end
   
-  def introduction(name)
-  puts "Hi, my name is #{name}."
-end
-
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
 end
