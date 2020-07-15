5f41c54256eae44519aaaaafbbfbb138913f3cbd
Lauren Stone <lstone321@gmail.com>
2020-07-15 00:49:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator (attendees)
   badges = []
   attendees.each do |name|
-    badges << ("Hello, my name is #{name})
+    badges << ("Hello, my name is #{name}.")
 end
   badges
 end
