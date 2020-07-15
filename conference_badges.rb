3134ab117719ca7f3262c3cb26f59c74b8cd46e9
Lauren Stone <lstone321@gmail.com>
2020-07-15 01:44:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ end
 end
 
 def printer(attendees)
-  batch_badge_creator (attendees).each do |badges|
+  batch_badge_creator(attendees).each do |badges|
     puts badges
 end
   assign_rooms(attendees).each do |rooms|
