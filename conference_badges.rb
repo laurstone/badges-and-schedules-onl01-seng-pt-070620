918ab60a29526b117e76b67e7bf3a0b417351bf5
Lauren Stone <lstone321@gmail.com>
2020-07-15 01:08:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 
 def assign_rooms(attendees)
   rooms = []
-rooms.each_with_index do |name,index|
+assign_rooms.each_with_index do |name,index|
   rooms << "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end
   rooms
